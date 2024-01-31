4ad704fa29b17f8593d46544f0cbfa1266a8d51a
Pedro Ferreira <pedrohsferreira1@gmail.com>
2024-01-31 11:25:16 -0300

    we are the champions! after months I finished the course, now is practice more and revise

--- graphs/prims_algo.cpp
+++ graphs/prims_algo.cpp
@@ -9,12 +9,12 @@ using namespace std;
 int cost [V][V] =  {
             {I, I, I, I, I, I, I, I},
             {I, I, 25, I, I, I, 5, I},
-            {I, 25, I, 12, I, I, I, 10},
-            {I, I, 12, I, 8, I, I, I},
-            {I, I, I, 8, I, 16, I, 14},
-            {I, I, I, I, 16, I, 20, 18},
-            {I, 5, I, I, I, 20, I, I},
-            {I, I, 10, I, 14, 18, I, I}};
+            {I, 25, I, 12, I, I, I, 6},
+            {I, I, 9, I, 10, I, I, I},
+            {I, I, I, 8, I, 12, I, 11},
+            {I, I, I, I, 12, I, 18, 16},
+            {I, 5, I, I, I, 18, I, I},
+            {I, I, 6, I, 11, 16, I, I}};
 
 int near[8] = {I, I, I, I, I, I, I, I};
 int t[2][7];
